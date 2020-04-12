c355f511aed403e70c2745b4718483ea7c6eb2e7
MJ Kubba <mjkubba@gmail.com>
2020-04-12 09:20:46 -0500

    fixed versioning

--- s3.py
+++ s3.py
@@ -8,8 +8,8 @@ from versioning import get_bucket_versioning
 
 def set_logger():
     """Set the logger."""
-    slog = logging.getLogger('s3scanner-screen')
-    slog.setLevel(logging.INFO)
+    logger = logging.getLogger('s3scanner-screen')
+    logger.setLevel(logging.INFO)
 
     level_styles = {
         'info': {'color': 'cyan'},
@@ -21,10 +21,10 @@ def set_logger():
         'asctime': {'color': 'white'}
         }
 
-    coloredlogs.install(level='DEBUG', logger=slog,
+    coloredlogs.install(level='DEBUG', logger=logger,
                         fmt='%(asctime)s   %(message)s',
                         level_styles=level_styles, field_styles=field_styles)
-    return slog
+    return logger
 
 
 def check_input():
@@ -50,34 +50,34 @@ def list_all_buckets(s3client):
 def main():
     """Main."""
     s3client = boto3.client('s3')
-    bucket, _ = check_input()
+    bucket, fix = check_input()
     results_file = open("s3results.csv", "w")
     results_file.write("Bucket Name,Versioning\n")
-    slog = set_logger()
+    logger = set_logger()
     bucket_list = list_all_buckets(s3client)
 
     if bucket:
         if bucket in bucket_list:
             get_bucket_versioning(bucket, s3client)
         else:
-            slog.error("Bucket Not Found: %s", bucket)
+            logger.error("Bucket Not Found: %s", bucket)
     else:
         num_buckets = len(bucket_list)
         counter = 1
-        slog.info("Found: %d Buckets", num_buckets)
+        logger.info("Found: %d Buckets", num_buckets)
         for bucket in bucket_list:
-            versioning = get_bucket_versioning(bucket, s3client)
-            print(versioning)
-            slog.info("%d of %d", counter, num_buckets)
+            versioning = get_bucket_versioning(bucket, s3client, logger)
+            logger.info(versioning)
+            logger.info("%d of %d", counter, num_buckets)
             results_file.write(bucket + "," + str(versioning) + "\n")
             counter = counter + 1
         #     if fix:
         #         if not ssl or not vpc:
-        #             slog.info("Fixing " + name + " Policy")
+        #             logger.info("Fixing " + name + " Policy")
         #             fix_policy(name, ssl, vpc, message)
         #             results_file.write("%d of %d", counter, num_buckets)
         #         else:
-        #             slog.info("Policy is good, nothing to fix")
+        #             logger.info("Policy is good, nothing to fix")
     results_file.close()
 
 
--- versioning.py
+++ versioning.py
@@ -1,109 +1,18 @@
 """DocString: Scan for buckets policies."""
-import logging
-import json
-import sys
-import boto3
-import coloredlogs
+
 from botocore.exceptions import ClientError
-s3 = boto3.client('s3')
 
 
-def set_logger():
-    """Set the logger."""
-    slog = logging.getLogger('s3scanner-screen')
-    slog.setLevel(logging.INFO)
-
-    levelStyles = {
-            'info': {'color': 'cyan'},
-            'warning': {'color': 'yellow'},
-            'error': {'color': 'red'}
-            }
-
-    fieldStyles = {
-            'asctime': {'color': 'white'}
-            }
-
-    coloredlogs.install(level='DEBUG', logger=slog,
-                        fmt='%(asctime)s   %(message)s',
-                        level_styles=levelStyles, field_styles=fieldStyles)
-    return slog
-
-
-def check_input():
-    """Check for inputs and exit if missing."""
-    fix = False
-    buck = False
-    if '-f' in sys.argv:
-        fix = True
-    if '-b' in sys.argv:
-        buck = sys.argv[sys.argv.index('-b') + 1]
-    return(buck, fix)
-
-
-def list_all_buckets():
-    """List all buckets in the AWS account."""
-    list_to_return = []
-    test = s3.list_buckets()
-    for bucket in test["Buckets"]:
-        list_to_return.append(bucket["Name"])
-    return list_to_return
-
-
-def get_bucket_versioning(bucket_name):
+def get_bucket_versioning(bucket_name, s3, logger):
+    """Get the bucket versioning."""
     try:
         versioning = s3.get_bucket_versioning(Bucket=bucket_name)
     except ClientError as e:
-        print(versioning)
-    print(versioning)
-
-
-def main():
-    """Main."""
-    bucket, fix = check_input()
-    f = open("s3Versioning.csv", "w")
-    f.write("Bucket Name,Versioning\n")
-    slog = set_logger()
-    bucket_list = list_all_buckets()
-
-    if bucket:
-        if bucket in bucket_list:
-            get_bucket_versioning(bucket)
-        else:
-            slog.error("Bucket Not Found: " + bucket)
+        logger.error(versioning, str(e))
+    if "Status" in versioning:
+        if versioning["Status"] == "Suspended":
+            return "Suspended"
+        elif versioning["Status"] == "Enabled":
+            return "Enabled"
     else:
-        num_buckets = len(bucket_list)
-        counter = 1
-        slog.info("Found: " + str(len(bucket_list)) + " Buckets")
-        for bucket in bucket_list:
-            get_bucket_versioning(bucket)
-        #     name, ssl, vpc, message = get_bucket_policy(bucket)
-        #     slog.info(str(counter) + " of " + str(num_buckets) + " " +
-        #               name + "," + str(ssl) + "," + str(vpc) + "," + message)
-        #     f.write(name + "," + str(ssl) + "," + str(vpc) + "," +
-        #             message + "\n")
-            counter = counter + 1
-        #     if fix:
-        #         if not ssl or not vpc:
-        #             slog.info("Fixing " + name + " Policy")
-        #             fix_policy(name, ssl, vpc, message)
-        #             f.write(name + "," + str(ssl) + "," + str(vpc) + "," +
-        #                     "Policy Fixed" + "\n")
-        #         else:
-        #             slog.info("Policy is good, nothing to fix")
-    f.close()
-
-
-def exit_gracefully():
-    """Handle ctrl+c."""
-    print("\n")
-    print("Things should be logged in s3results.csv file")
-    print("\n")
-
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        exit_gracefully()
+        return "None"
